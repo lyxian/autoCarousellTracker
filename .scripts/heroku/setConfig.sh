55f0e5d3df29317c8a870b50b8bf8a245b92dea1
Lyx <Lyxlyxi@hotmail.com>
2021-05-06 14:24:09 +0800

    add whoami, .scripts

---
+++ .scripts/heroku/setConfig.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+files=$(ls .config | grep .txt)
+
+for file in ${files[@]}
+do
+if [[ $(echo $file | cut -d "." -f 1) == "key" ]]
+then
+    key=$(echo $file | cut -d "." -f 1 | tr a-z A-Z)
+else
+    key=$(echo $file | cut -d "." -f 1 | cut -d "_" -f 2 | tr a-z A-Z)_KEY
+fi 
+if [[ $(heroku config:set --app $(cat whoami) $key=$(cat .config/$file) $key) ]]
+then
+echo "Sucess...$key set"
+else
+echo "Error...$key not set"
+else
+fi
+done 
