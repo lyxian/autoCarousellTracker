e3e3b2be0661ca46ad417fa9ac4cfcbd58569ef7
Lyx <Lyxlyxi@hotmail.com>
2021-05-06 15:48:44 +0800

    update main.py, whoami

--- AutoCarousell_staging.py
+++ AutoCarousell_staging.py
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     while True:
         # Perform Handover
         print(f'Date now is: {datetime.now().day}...')
-        if datetime.now().day % 2 == 0:
+        if datetime.now().day % 2 == 1:
             next_app = App(f'yxian-carousell-{2-(order+1)%2}')
             next_app.enable(True)
             curr_app = App(f'yxian-carousell-{order}')
