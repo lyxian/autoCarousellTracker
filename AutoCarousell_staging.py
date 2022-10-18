cfea655f412863e122ed99ba223cec69a760a458
lyx <yxianlai@hotmail.com>
2022-10-19 03:59:08 +0800

    v8/fix: debug server

--- AutoCarousell_staging.py
+++ AutoCarousell_staging.py
@@ -8,4 +8,4 @@ import time
 
 if __name__ == '__main__':
     app = hostServer()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)), debug=True)
